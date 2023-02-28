1545e826d0f1b12d05e0138d31352c9f5c0ab701
BaoLam <baolam.nbl@gmail.com>
2023-02-28 14:58:17 +0700

    mapping model class to table db

--- TatBlog.Core/Entities/Post.cs
+++ TatBlog.Core/Entities/Post.cs
@@ -24,5 +24,6 @@ namespace TatBlog.Core.Entities
         public Category Category { get; set; }
         public Author Author { get; set; }
         public IList<Tag> Tags { get; set; }
+        
     }
 }
